7a0122f00ba88f7bfeac86e4e1380223c196d443
Aleksandra Nenadic <anenadic@manchester.ac.uk>
2015-05-20 15:21:03 +0100

    Another attempt to add training material url field to dataset.

--- ckanext/tess/dataset.py
+++ ckanext/tess/dataset.py
@@ -11,41 +11,42 @@ class DatasetPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     #         'training_material_url': [toolkit.get_validator('ignore_missing'), toolkit.get_validator('url_validator'), toolkit.get_converter('convert_to_extras')]
     #     })
     #     return schema
-    #
-    # def create_package_schema(self):
-    #     # grab the default schema
-    #     schema = super(DatasetPlugin, self).create_package_schema()
-    #     schema = self._modify_package_schema(schema)
-    #     return schema
-    #
-    # def update_package_schema(self):
-    #     # grab the default schema
-    #     schema = super(DatasetPlugin, self).update_package_schema()
-    #     schema = self._modify_package_schema(schema)
-    #     return schema
-    #
-    #
-    # def show_package_schema(self):
-    #     schema = super(DatasetPlugin, self).show_package_schema()
-    #     schema.update({
-    #         'training_material_url': [toolkit.get_converter('convert_from_extras'), toolkit.get_validator('check_missing')]
-    #     })
-    #     return schema
+
+    def create_package_schema(self):
+        # grab the default schema
+        schema = super(DatasetPlugin, self).create_package_schema()
+        schema = self._modify_dataset_schema(schema)
+        return schema
+
+
+    def update_package_schema(self):
+        # grab the default schema
+        schema = super(DatasetPlugin, self).update_package_schema()
+        schema = self._modify_dataset_schema(schema)
+        return schema
+
+
+    def show_package_schema(self):
+        return db_to_form_schema(self)
+
 
     def form_to_db_schema_api_create(self):
         schema = super(DatasetPlugin, self).form_to_db_schema_api_create()
         schema = self._modify_dataset_schema(schema)
         return schema
 
+
     def form_to_db_schema_api_update(self):
         schema = super(DatasetPlugin, self).form_to_db_schema_api_update()
         schema = self._modify_dataset_schema(schema)
         return schema
 
+
     def form_to_db_schema(self):
         schema = super(DatasetPlugin, self).form_to_db_schema()
         schema = self._modify_dataset_schema(schema)
         return schema
+
 
     def _modify_dataset_schema(self, schema):
          #Import core converters and validators
@@ -58,6 +59,7 @@ class DatasetPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
             'training_material_url': [_not_missing, _not_empty, _url_validator, _convert_to_extras]
         })
         return schema
+
 
     def db_to_form_schema(self):
         # Import core converters and validators
