e8743dff0cdba6078b8d810d03ca842bbfca956b
Niall Beard <niallbeard@gmail.com>
2015-06-05 17:05:42 +0100

    Improved modal layout and information for read and edit modes

--- ckanext/tess/workflow.py
+++ ckanext/tess/workflow.py
@@ -111,8 +111,9 @@ class WorkflowController(HomeController):
         return base.render('workflow/ajax/read_training.html')
 
     def edit_training(self):
+        print
         c.training_materials = training_material_options()
-
+        c.stage_name
         return base.render('workflow/ajax/edit_training.html')
 
 
