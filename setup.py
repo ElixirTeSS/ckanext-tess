56fa352d2fcdd6f420e84031a0968f1ff350d5e7
Niall Beard <niallbeard@gmail.com>
2015-07-20 16:58:19 +0100

    entry point to new Workflow API action

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         organization=ckanext.tess.organization:OrganizationPlugin
         workflow=ckanext.tess.workflow:WorkflowPlugin
         workflow_controller=ckanext.tess.workflow:WorkflowController
+        workflow_api=ckanext.tess.workflow:WorkflowApi
         event=ckanext.tess.event:EventPlugin
         event_controller=ckanext.tess.event:EventController
         event_api=ckanext.tess.event:EventApi
